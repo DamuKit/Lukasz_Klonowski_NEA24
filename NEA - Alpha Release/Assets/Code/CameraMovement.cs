0a777ef049dbb800e4c658abee1ca8660c9d3ae5
LukaszMaple <118005385+LukaszMaple@users.noreply.github.com>
2023-09-19 12:11:50 +0100

    Sprint 1.5
    Dashing

---
+++ NEA - Alpha Release/Assets/Code/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	public int locX;
+	public int locY;
+	public GameObject Player;
+	public PlayerMovement playerMovement;
+	// Use this for initialization
+	void Start () {
+		locX = 0;
+		locY = 0;
+		//Debug.Log ("E");
+		//Debug.Log (playerMovement.camerasizex);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (Player.transform.position.y + " " + ((locY) * 2 * playerMovement.camerasizey + playerMovement.camerasizey) + " " + ((locY) * 2 * playerMovement.camerasizey - playerMovement.camerasizey));
+		if (Player.transform.position.y > ((locY) * 2 * playerMovement.camerasizey + playerMovement.camerasizey)) {
+			locY += 1 ;
+			this.gameObject.transform.SetPositionAndRotation (new Vector3 (((locX) * 2 * playerMovement.camerasizex), ((locY) * 2 * playerMovement.camerasizey), -10), new Quaternion (0, 0, 0, 0));
+		}
+		if (Player.transform.position.y < ((locY) * 2 * playerMovement.camerasizey - playerMovement.camerasizey)) {
+			locY -= 1 ;
+			this.gameObject.transform.SetPositionAndRotation (new Vector3 (((locX) * 2 * playerMovement.camerasizex), ((locY) * 2 * playerMovement.camerasizey), -10), new Quaternion (0, 0, 0, 0));
+		}
+		if (Player.transform.position.x > ((locX) * 2 * playerMovement.camerasizex + playerMovement.camerasizex)) {
+			locX += 1 ;
+			this.gameObject.transform.SetPositionAndRotation (new Vector3 (((locX) * 2 * playerMovement.camerasizex), ((locY) * 2 * playerMovement.camerasizey), -10), new Quaternion (0, 0, 0, 0));
+		}
+		if (Player.transform.position.x < ((locX) * 2 * playerMovement.camerasizex - playerMovement.camerasizex)) {
+			locX -= 1;
+			this.gameObject.transform.SetPositionAndRotation (new Vector3 (((locX) * 2 * playerMovement.camerasizex), ((locY) * 2 * playerMovement.camerasizey), -10), new Quaternion (0, 0, 0, 0));
+		}
+
+	}
+}
--- NEA - Alpha Release/Assets/Code/PlayerMovement.cs
+++ NEA - Alpha Release/Assets/Code/PlayerMovement.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+	float angle;
 	float speed;
 	Animator Animation;
+	public CameraMovement camMov;
 	string direction;
 	bool moving;
-	float camerasizex;
-	float camerasizey;
+	bool dashing;
+	public float camerasizex;
+	public float camerasizey;
 	// Use this for initialization
 	void Start () {
 		speed = 0.05f;
 		direction = "down";
 		moving = false;
+		dashing = false;
 		camerasizex = 12f;
 		camerasizey = 8f;
 		Animation = GetComponent<Animator>();
@@ -32,7 +39,7 @@ public class PlayerMovement : MonoBehaviour {
 			moving = true;
 			Animation.SetBool ("walk", true);
 		}
-			
+
 		if (Input.GetKey (KeyCode.W) == true) {
 			this.transform.Translate (0, speed, 0);
 			moving = true;
@@ -42,47 +49,77 @@ public class PlayerMovement : MonoBehaviour {
 			moving = true;
 			Animation.SetBool ("walk", true);
 		}
+		if (Input.GetKeyDown (KeyCode.LeftShift) == true) {
+			dashing = true;
+			Animation.SetBool ("dash", true);
+			Animation.Play ("Dash");
+		}
+
+		if(dashing = true){
+			moving = true;
+		}
+
 
 		if(Input.mousePosition.x-(Display.main.systemWidth/2)>=0 & Input.mousePosition.y-(Display.main.systemHeight/2)>= 0)
 		{
-			Debug.Log ("e");
+			//Debug.Log ("e");
 		}
-		if (Input.mousePosition.x - (Display.main.systemWidth / 2) - ((this.transform.position.x / camerasizex)* (Display.main.systemWidth/2))  > Mathf.Abs (Input.mousePosition.y - (Display.main.systemHeight / 2) - ((this.transform.position.y / camerasizey)* (Display.main.systemHeight/2)))) {
+
+		Angle();
+		Debug.Log (angle);
+		if (angle >= 45 && angle <= 135) {
 			//Debug.Log((this.transform.position.x / camerasizex)* (Display.main.systemWidth/2));
 			//Debug.Log (Input.mousePosition.x - (Display.main.systemWidth / 2));
 			direction = "right";
-			Debug.Log ("Right");
+			//Debug.Log ("Right");
 			Animation.SetBool ("right", true);
 			Animation.SetBool ("left", false);
 			Animation.SetBool ("up", false);
 			Animation.SetBool ("down", false);
 
-		} else if (Input.mousePosition.x - (Display.main.systemWidth / 2) - ((this.transform.position.x / camerasizex)* (Display.main.systemWidth/2)) < -Mathf.Abs (Input.mousePosition.y - (Display.main.systemHeight / 2) - ((this.transform.position.y / camerasizey)* (Display.main.systemHeight/2)))) {
+		} else if (angle >= 225 && angle <= 315) {
 			direction = "left";
-			Debug.Log ("Left");
+			//Debug.Log ("Left");
 			Animation.SetBool ("left", true);
 			Animation.SetBool ("right", false);
 			Animation.SetBool ("up", false);
 			Animation.SetBool ("down", false);
 
-		} else if (Mathf.Abs(Input.mousePosition.x - (Display.main.systemWidth / 2) - ((this.transform.position.x / camerasizex)* (Display.main.systemWidth/2))) < Input.mousePosition.y - (Display.main.systemHeight / 2)  - ((this.transform.position.y / camerasizey)* (Display.main.systemHeight/2))) {
+		} else if (angle >= 315 | angle <= 45) {
 			direction = "up";
-			Debug.Log ("up");
+			//Debug.Log ("up");
 			Animation.SetBool ("up", true);
 			Animation.SetBool ("right", false);
 			Animation.SetBool ("left", false);
 			Animation.SetBool ("down", false);
 
-		} else if (-Mathf.Abs(Input.mousePosition.x - (Display.main.systemWidth / 2) - ((this.transform.position.x / camerasizex)* (Display.main.systemWidth/2))) > Input.mousePosition.y - (Display.main.systemHeight / 2)  - ((this.transform.position.y / camerasizey)* (Display.main.systemHeight/2))) {
+		} else if (angle >= 135 && angle <= 225) {
 			direction = "down";
-			Debug.Log ("down");
+			//Debug.Log ("down");
 			Animation.SetBool ("down", true);
 			Animation.SetBool ("right", false);
 			Animation.SetBool ("left", false);
 			Animation.SetBool ("up", false);
 
 		}
+		//-(camMov.locX * camerasizex)
+		//-(camMov.locY * camerasizey) (((this.transform.position.y -(camMov.locY * camerasizey))/ camerasizey)
 
 
+
+	}
+	public void Angle () {
+		if (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2)) >= 0 & 0 <= Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2))) {
+			angle = (Mathf.Atan (Mathf.Abs (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2))) / (Mathf.Abs (Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2))))) * Mathf.Rad2Deg);
+		}
+		else if (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2)) >= 0 & 0 >= Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2))) {
+			angle = (90 + Mathf.Atan ((Mathf.Abs (Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2)))) / Mathf.Abs (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2)))) * Mathf.Rad2Deg);
+		}
+		else if (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2)) <= 0 & 0 >= Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2))) {
+			angle = (180 + Mathf.Atan (Mathf.Abs (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2))) / (Mathf.Abs (Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2))))) * Mathf.Rad2Deg);
+		}
+		else if (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2)) <= 0 & 0 <= Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2))) {
+			angle = (270 + Mathf.Atan ((Mathf.Abs (Input.mousePosition.y - (Display.main.systemHeight / 2) - (((this.transform.position.y - (camMov.locY * camerasizey * 2)) / camerasizey) * (Display.main.systemHeight / 2)))) / Mathf.Abs (Input.mousePosition.x - (Display.main.systemWidth / 2) - (((this.transform.position.x - (camMov.locX * camerasizex * 2)) / camerasizex) * (Display.main.systemWidth / 2)))) * Mathf.Rad2Deg);
+		}
 	}
 }
