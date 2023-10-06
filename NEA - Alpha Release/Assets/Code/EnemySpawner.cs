ca0b9b78ffdcf70565ca189a89257b3bb459561f
LukaszMaple <118005385+LukaszMaple@users.noreply.github.com>
2023-10-06 08:57:23 +0100

    3.4

--- NEA - Alpha Release/Assets/Code/EnemySpawner.cs
+++ NEA - Alpha Release/Assets/Code/EnemySpawner.cs
@@ -10,19 +10,34 @@ public class EnemySpawner : MonoBehaviour {
 	public int points;
 	public int room;
 	public int seed;
+	string roomlocation;
+	public CameraMovement camMov;
+	public List<string> Locations = new List<string>();
+	public GameObject Cam;
+
 
 	// Use this for initialization
 	void Start () {
+		Cam = GameObject.FindGameObjectWithTag("MainCamera");
+		camMov = Cam.GetComponent<CameraMovement> ();
 		seed = 51;
 		Random.InitState (seed);
 		Debug.Log (Random.value);
 		Debug.Log (Random.value);
 		Debug.Log (Random.value);
-		l
+		//Locations.Add ("0.0");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		roomlocation = camMov.locX.ToString() + "." + camMov.locY.ToString();
+		Debug.Log (Locations.FindIndex (a => a == roomlocation));
+		if (Locations.FindIndex(a => a == roomlocation) != -1) {
+			Debug.Log ("e");
+		} else {
+			Locations.Add (roomlocation);
+			Debug.Log (Locations [Locations.Count - 1]);
+			Debug.Log (Locations.Count - 1);
+		}			
 	}
 }
