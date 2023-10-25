9be988c48889ab49defcbe9796134485451af2c0
LukaszMaple <118005385+LukaszMaple@users.noreply.github.com>
2023-10-25 11:53:01 +0100

    4.8

--- NEA - Alpha Release/Assets/Resources/Code/TextInputField.cs
+++ NEA - Alpha Release/Assets/Resources/Code/TextInputField.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using TMPro;
 public class TextInputField : MonoBehaviour {
 	public TMP_InputField input;
+	public TextboxController textbox;
 	string text;
 	// Use this for initialization
 	void Start () {
 		input = this.GetComponent<TMPro.TMP_InputField> ();
+		textbox = GameObject.Find ("Textbox").GetComponent<TextboxController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Return) == true & input.text != "") {
 			text = input.text;
+			textbox.textbox.Add (text);
 			Debug.Log (text);
 			input.text = "";
 		}
--- NEA - Alpha Release/Assets/Resources/Code/TextboxController.cs
+++ NEA - Alpha Release/Assets/Resources/Code/TextboxController.cs
@@ -5,7 +5,8 @@ using TMPro;
 
 public class TextboxController : MonoBehaviour {
 	public List<string> textbox = new List<string> ();
-	public TextMeshPro chat;
+	public TMP_Text chat;
+	string processSaver;
 	// Use this for initialization
 
 	//12 spaces shown at a time
@@ -14,12 +15,52 @@ public class TextboxController : MonoBehaviour {
 
 
 	void Start () {
-		text = "a/na/na/na/na/na/na/na/na/na/na/na";
-		chat = this.GetComponent<TextMeshPro> ();
+		//text = textbox[textbox.Capacity];
+		chat = this.GetComponent<TMPro.TMP_Text> ();
+		textbox.Add("W, A, S, D to move");
+		textbox.Add("Left mouse to attack");
+		textbox.Add("Left Shift to dash");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		textbox.Add("");
+		processSaver = "e";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		chat.SetText(text);
+		if(processSaver != (textbox [textbox.Count - 1]).ToString()){
+			processSaver = (textbox [textbox.Count - 1]).ToString();
+
+			chat.text = textbox[textbox.Count-12] + "\n" + textbox[textbox.Count-11] + "\n" + textbox[textbox.Count-10] + "\n" + textbox[textbox.Count-9] + "\n" + textbox[textbox.Count-8] + "\n" + textbox[textbox.Count-7] + "\n" + textbox[textbox.Count-6] + "\n" + textbox[textbox.Count-5] + "\n" + textbox[textbox.Count-4] + "\n" + textbox[textbox.Count-3] + "\n" + textbox[textbox.Count-2] + "\n" + textbox[textbox.Count-1];
+			if ((textbox [textbox.Count - 1]).ToString ().ToLower () == "/help") {
+				textbox.Add ("No help for you.");
+			} else if ((textbox [textbox.Count - 1]).ToString ().ToLower () == "/clearchat") {
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+				textbox.Add ("");
+			} else if ((textbox [textbox.Count - 1]).ToString ().ToLower ().Substring (0, 5) == "/heal") {
+				if ((textbox [textbox.Count - 1]).ToString ().ToLower ().Substring (0, 6) == "/heal ") {
+					textbox.Add ("Okay cheater");
+				} else {
+					textbox.Add ("error");
+				}
+			}
+		}
+
 	}
 }
