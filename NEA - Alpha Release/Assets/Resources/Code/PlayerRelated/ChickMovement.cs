d30078ed2e3792c3566299a9015aa5dd71ada40b
LukaszMaple <118005385+LukaszMaple@users.noreply.github.com>
2023-12-22 09:01:55 +0000

    8.1

--- NEA - Alpha Release/Assets/Resources/Code/PlayerRelated/ChickMovement.cs
+++ NEA - Alpha Release/Assets/Resources/Code/PlayerRelated/ChickMovement.cs
@@ -6,21 +6,64 @@ public class ChickMovement : MonoBehaviour {
 	public float angle;
 	public float speed;
 	public float baseSpeed;
-
+	public float Damage;
 	StatsStorage stats;
 	PetInteraction interaction;
+	PlayerMovement player;
+	public CameraMovement camMov;
+	string location;
+	bool wonder;
+	int delay;
 	// Use this for initialization
 	void Start () {
+		camMov = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement> ();
+		player = GameObject.Find ("Player").GetComponent<PlayerMovement> ();
 		stats = GameObject.Find ("PassiveCodeController").GetComponent<StatsStorage> ();
-		interaction = GameObject.Find ("PetInteraction").GetComponent<PetInteraction> ();
-		baseSpeed = 3f;
+		interaction = this.gameObject.GetComponent<PetInteraction> ();
+		baseSpeed = 2.5f;
 		angle = -10;
+		Damage = 5;
+		location = "0.0";
+		wonder = false;
+		delay = 0;
+		Random.InitState (stats.seed + stats.seedoffset);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		angle = interaction.angle;
+		
+
+		
 		speed = baseSpeed * 0.5f * Time.deltaTime * stats.pause;
+		if (wonder == true) {
+			speed *= 0.5f;
+		}
+
+		if (location != (camMov.locX + "." + camMov.locY)) {
+			location = (camMov.locX + "." + camMov.locY);
+			this.gameObject.transform.position = player.transform.position;
+		}
+
+		RaycastHit2D DetectPlayer = Physics2D.Raycast (this.gameObject.transform.position - new Vector3(0, 0.25f), (player.transform.position - transform.position - new Vector3(0, 0.1f))*2);
+		if (delay > 0) {
+			delay--;
+			if (delay <= 0) {
+				wonder = false;
+			}
+		}
+		else if (interaction.focusDistance <= 5) {
+			angle = interaction.angle;
+		}
+		else if(DetectPlayer.distance <= 10 & DetectPlayer.distance > 1f){
+			angle = Mathf.Rad2Deg * (Mathf.Atan (Mathf.Abs ((0.5f + 0.5f * Mathf.Sign (player.transform.position.x - this.transform.position.x) * Mathf.Sign (player.transform.position.y - this.transform.position.y)) * (player.transform.position.x - this.transform.position.x) / (player.transform.position.y - this.transform.position.y) + ((0.5f + 0.5f * -Mathf.Sign (player.transform.position.x - this.transform.position.x) * Mathf.Sign (player.transform.position.y - this.transform.position.y))) * (player.transform.position.y - this.transform.position.y) / (player.transform.position.x - this.transform.position.x)))) + 45 * (2 - 2 * Mathf.Sign (player.transform.position.x - this.transform.position.x) + 1 - Mathf.Sign (player.transform.position.x - this.transform.position.x) * Mathf.Sign (player.transform.position.y - this.transform.position.y));
+
+		}
+		else{
+			delay = Random.Range(40, 100);
+			angle = Random.Range(-100, 360);
+			wonder = true;
+		}
+
 
 		if (angle <= 360 & angle >= 337.5 | angle <= 22.5 & angle >= 0) {
 			this.transform.Translate (0, speed, 0);
@@ -42,5 +85,13 @@ public class ChickMovement : MonoBehaviour {
 
 	}
 
+	private void OnCollisionStay2D(Collision2D other) {
+		if (other.gameObject.tag == "Enemy") {
+			
+			other.gameObject.SendMessage ("damaged", (Damage)/** (1 + damagebuff * 0.25f)*/);
+
+			//Destroy (other.gameObject);
+		}
+	}
 
 }
--- NEA - Alpha Release/Assets/Resources/Code/PlayerRelated/InformPet.cs
+++ NEA - Alpha Release/Assets/Resources/Code/PlayerRelated/InformPet.cs
@@ -8,28 +8,36 @@ public class InformPet : MonoBehaviour {
 	PetInteraction petInfo;
 	float distance;
 	float angle;
+	Vector3 position;
 	// Use this for initialization
 
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		try{
 			pet = GameObject.FindGameObjectWithTag("Pet");
-			RaycastHit2D DetectPlayer = Physics2D.Raycast (this.gameObject.transform.position, pet.transform.position - this.transform.position, 5);
-			if(DetectPlayer.collider.tag=="Pet"){
-				petInfo = DetectPlayer.collider.GetComponent<PetInteraction> ();
+
+			position = pet.transform.position + new Vector3(0,-0.25f,0);
+
+			RaycastHit2D DetectPet = Physics2D.Raycast (this.gameObject.transform.position, position - this.transform.position , 15);
+			//Debug.DrawRay(this.gameObject.transform.position, position - this.transform.position , Color.blue, 15);
+			//Debug.Log(DetectPet.collider.name);
+			if(DetectPet.collider.tag=="Pet"){
+				petInfo = DetectPet.collider.GetComponent<PetInteraction> ();
 
 
-				distance = Mathf.Sqrt(Mathf.Pow(DetectPlayer.collider.transform.position.x - this.gameObject.transform.position.x, 2) + Mathf.Pow(DetectPlayer.collider.transform.position.y - this.gameObject.transform.position.y, 2));
+				distance = Mathf.Sqrt(Mathf.Pow(DetectPet.collider.transform.position.x - this.gameObject.transform.position.x, 2) + Mathf.Pow(DetectPet.collider.transform.position.y - this.gameObject.transform.position.y, 2));
+				//angle = Vector2.Angle(DetectPet.collider.transform.position, this.gameObject.transform.position);
+
 
 				//angle = enemy.angle;
 
 				angle = Mathf.Rad2Deg * (Mathf.Atan (Mathf.Abs ((0.5f + 0.5f * Mathf.Sign (this.transform.position.x - pet.transform.position.x) * Mathf.Sign (this.transform.position.y - pet.transform.position.y)) * (this.transform.position.x - pet.transform.position.x) / (this.transform.position.y - pet.transform.position.y) + ((0.5f + 0.5f * -Mathf.Sign (this.transform.position.x - pet.transform.position.x) * Mathf.Sign (this.transform.position.y - pet.transform.position.y))) * (this.transform.position.y - pet.transform.position.y) / (this.transform.position.x - pet.transform.position.x)))) + 45 * (2 - 2 * Mathf.Sign (this.transform.position.x - pet.transform.position.x) + 1 - Mathf.Sign (this.transform.position.x - pet.transform.position.x) * Mathf.Sign (this.transform.position.y - pet.transform.position.y));
 
-				DetectPlayer.collider.GetComponent<PetInteraction>().BroadcastMessage("Focus", new float[] {distance, angle});
+				DetectPet.collider.GetComponent<PetInteraction>().BroadcastMessage("Focus", new float[] {distance, angle});
 
 			}
 		}
